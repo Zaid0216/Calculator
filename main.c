47b36288ace92f1fcb3d1b3a637e547d0d9f1521
Zaid0216 <khanmuhammadzaid91@gmail.com>
2024-11-08 12:14:19 +0500

    my calculator

---
+++ main.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+int main() {
+    char operator;
+    float n1, n2, result;
+
+    printf("Enter a operator (+, -, *, /): ");
+    scanf("%c", &operator);
+
+    printf("Enter 1st number: ");
+    scanf("%f", &n1);
+
+    printf("Enter 2st number: ");
+    scanf("%f", &n2);
+
+    switch(operator){
+        case'+':
+            result = n1 + n2;
+            printf("%.2f + %.2f = %.2f", n1, n2, result);
+            break;
+
+        case'-':
+            result = n1 - n2;
+            printf("%.2f - %.2f = %.2f", n1, n2, result);
+            break;
+
+        case '*':
+            result = n1 * n2;
+            printf("%.2f * %.2f = %.2f", n1, n2, result);
+            break;
+
+        case '/':
+            if(n2 != 0){
+                 result = n1 / n2;
+                 printf("%.2f / %.2f = %.2f", n1, n2, result);
+             }
+             else{
+                printf("Cannot be divisible by 0");
+             }
+            break;
+
+        default:
+            printf("Enter a correct operatorr");
+            break;
+    }
+    return 0;
+}
